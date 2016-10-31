3f92be3af7d9f08501e574fbbf64ae51235cac56
joytsai626 <105021013@live.asia.edu.tw>
2016-10-31 16:01:59 +0800

    ex
    ex

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -5,14 +5,26 @@
  @@@
  @@@
  * Date: 2016/10/31
- * Author: 1050210XX 周永振老師
+ * Author: 105021013 周永振老師
  */
-
+import java.util.Scanner;
 public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+	System.out.println(n+","+"'@'");
+    fun1(n);}
+    public static void fun1(int n){
+	 int sum = 0;
+	 for(int i=0;i<n;i++)
+	 {
+	 for(int j=0;j<n;j++)
+	 {
+	 System.out.print('@');	
+	 }
+	 System.out.println( );
+	 }
+	 }
 	}
-
-}
--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -2,14 +2,24 @@
 /*
  * Topic: 使用者輸入兩個整數，透過函數呼叫輸出排列組合函數 C(m,n)。公式: C(m,n) = m!/n!(m-n)!
  * Date: 2016/10/31
- * Author: 1050210XX 周永振老師
+ * Author: 105021013 周永振老師
  */
-
+import java.util.Scanner;
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+    Scanner scn = new Scanner(System.in);
+    System.out.println("輸入m和n:");
+    int m = scn.nextInt();
+    int n = scn.nextInt();
+    fun1(n);}
+	public static void fun1(int n){
+		int sum = 0;
+		for(int i=0;i<n;i++){
+			
+		}
+    
 	}
 
 }
