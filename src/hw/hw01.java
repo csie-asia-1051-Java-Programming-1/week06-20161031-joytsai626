fa1dc3e260cf6417fbe49230417ebe72cb6adaf9
joytsai626 <105021013@live.asia.edu.tw>
2016-11-02 00:16:09 +0800

    hw
    hw

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,30 @@
 package hw;
-
+import java.util.*;
 public class hw01 {
+/*請設計一個函數fun(v1, type)，讓使用者輸入一個溫度值及計算方式(type 為1 時 華氏->攝氏，type為2時攝氏->華氏)，
+ * 函數算出結果後要回傳至主程式再印出結果。(F=C*9/5+32), (C = (F-32) * (5/9)，
+ * 例如輸入100 1 則輸出 37.77778; 輸入 37.77778 2 時則輸出 100.0
+ */
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+       Scanner scn = new Scanner(System.in);
+       System.out.println("請輸入溫度");
+       float n =scn.nextFloat();
+       System.out.println("輸入1為華氏->攝氏");
+       System.out.println("輸入2為攝氏->華氏");
+       float a = scn.nextFloat();
+       float b = fun1(n,a);
+       System.out.println(b);
+	}
+	public static float fun1(float n1,float a1){
+		float sum = 0;
+	   if(a1 == 1){
+		   sum = (n1-32) * (5/9);
+	   }else{
+		   sum = (n1*9/5)+32;
+	   }
+	   return sum;
 	}
 
 }
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,28 @@
 package hw;
-
+import java.util.*;
 public class hw02 {
-
+/*讓使用者輸入性別與身高，透過函數呼叫，依據男女不同, 幫她(他)計算並輸出其標準體重
+ * ((1)男：(身高-170)*0.6+62 , (2)女：(身高-158)*0.5+52)。
+ * 函數算出結果後要回傳至主程式再印出結果。例如:輸入 1 170 則輸出 62.0, 輸入 2 165 則輸出 55.5
+ */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+       Scanner scn = new Scanner(System.in);
+       System.out.println("輸入性別:1.男生 2.女生");
+       int a = scn.nextInt();
+       System.out.println("輸入身高(cm)");
+       int n = scn.nextInt();
+       float b = fun1(a,n);
+       float sum = 0;
+       System.out.println(b);
 	}
-
+    public static float fun1(float a1,float n1){
+    	float sum = 0;
+    	if(a1 == 1){
+    		sum = (float) ((n1-170)*0.6+62);
+    	}else{
+    		sum = (float) ((n1-158)*0.5+52);
+    	}
+    return sum;
+}
 }
